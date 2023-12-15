f5d15a536d533dbefe8965628b587a45841f38b4
SuchindraKumar <SuchindraKumar.suchindra057@gmail.com>
2023-12-15 15:07:01 +0530

    signlanguage_detection

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Suchindra_Kumar',
+    author_email= 'suchindra057@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
